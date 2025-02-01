a5b69aaa2ed13a1e845aabdc3d59219aa09f9884
Madhan-1889 <madhans1889@gmail.com>
2025-02-01 16:29:16 +0530

    commit to dev
    commit

---
+++ wipro_java_project/src/com/wipro/java/Flowers.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class Flowers {
+
+	
+	String color;
+	String name;
+	
+	public Flowers(String name,String color) {
+		this.name= name;
+		this.color= color;
+	}
+	
+	public void display() {
+		System.out.println(name+" is the name of a flower and it is "+color+" in colour");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Flowers flower1= new Flowers("Rose","red");
+		Flowers flower2= new Flowers("Sunflower","yellow");
+		Flowers flower3= new Flowers("Jasmine","white");
+		flower1.display();
+		flower2.display();
+		flower3.display();
+
+	}
+
+}
