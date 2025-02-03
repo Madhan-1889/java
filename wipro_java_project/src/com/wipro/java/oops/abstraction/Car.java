ad017f3c1ed3d3cf41f1160d1ae4c0ab96977ff2
Madhan-1889 <madhans1889@gmail.com>
2025-02-03 15:44:08 +0530

    day2

--- wipro_java_project/src/com/wipro/java/oops/abstraction/Car.java
+++ wipro_java_project/src/com/wipro/java/oops/abstraction/Car.java
@@ -3,11 +3,12 @@
  */
 package com.wipro.java.oops.abstraction;
 
-/**
+/** Abstract class to achieve showing functionalities and hiding implementation
  * 
  */
 public abstract class Car {
 	
+	//abstract method with no implementation
 	public abstract void typeOfCar();
 
 }
--- wipro_java_project/src/com/wipro/java/oops/polymorphism/Maruti.java
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/Maruti.java
@@ -3,15 +3,18 @@
  */
 package com.wipro.java.oops.polymorphism;
 
-/**
- * 
+/** parent class= Car
+ *  Child Class = Maruti
+ *  keyword - extend
  */
 public class Maruti extends Car{
 	
 	public static void main(String a[]) {
-		Car obj= new Maruti();
-		obj.mileageCalculation(14);
-		obj.mileageCalculation(100,1.3f);
+		
+		//To achieve polymorphism here we used two method with same name but different parameters
+		Car obj= new Maruti(); // Creating object reference to access the class 
+		obj.mileageCalculation(14); //calling the method by passing single argument with integer data type
+		obj.mileageCalculation(100,1.3f);//calling method with two arguments
 	}
 	
 }
